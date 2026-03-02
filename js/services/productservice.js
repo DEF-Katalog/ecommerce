3cee6bd5199c49ef290a115cbac8484c1751167f
DEF-Katalog <def.bandungjuara@gmail.com>
2026-03-02 12:19:05 +0700

    Create productservice.js

---
+++ js/services/productservice.js
@@ -0,0 +1,22 @@
+import { db } from "../firebase.js";
+import { ref, push, set, onValue }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+export default class ProductService {
+
+  constructor() {
+    this.productsRef = ref(db, "products");
+  }
+
+  save(product) {
+    const newProductRef = push(this.productsRef);
+    return set(newProductRef, product);
+  }
+
+  listen(callback) {
+    onValue(this.productsRef, (snapshot) => {
+      callback(snapshot.val());
+    });
+  }
+
+}
