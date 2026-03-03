8c9215addf31f1f8101da2ff95adf862aded799d
DEF-Katalog <def.bandungjuara@gmail.com>
2026-03-03 12:38:52 +0700

    Update Variant.js

--- models/Variant.js
+++ models/Variant.js
@@ -1,1 +1,8 @@
+// Supaya tidak terjadi NaN atau string harga
 
+export default class Variant {
+  constructor(nama, harga) {
+    this.nama = nama;
+    this.harga = Number(harga); // pastikan number
+  }
+}
