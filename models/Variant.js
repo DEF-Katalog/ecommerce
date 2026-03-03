f3e733d45d275755ad4ebc80696997fb20818022
DEF-Katalog <def.bandungjuara@gmail.com>
2026-03-03 13:10:20 +0700

    Delete models directory

--- models/Variant.js
+++
@@ -1,8 +0,0 @@
-// Supaya tidak terjadi NaN atau string harga
-
-export default class Variant {
-  constructor(nama, harga) {
-    this.nama = nama;
-    this.harga = Number(harga); // pastikan number
-  }
-}
